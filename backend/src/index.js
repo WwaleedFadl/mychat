759acea9b32711644eb0fc6f0e4f6b7809127aee
waleedfadl <wwaleedfadl@gmail.com>
2025-04-19 21:04:20 +0200

    secure until now

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+import cookieParser from 'cookie-parser'
+import express from 'express';
+
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+
+import dotenv from 'dotenv'
+import { connectDB } from './lib/db.js';
+
+import cors from 'cors'
+
+dotenv.config()
+
+////////////// MIDDLEWARES \\\\\\\\\
+//
+const app = express()
+const PORT = process.env.PORT;
+
+app.use(express.json())
+
+app.use(cookieParser())
+app.use(cors({
+  origin: "http://localhost:5173"
+  , credentials: true
+}))
+
+////////////// ROUTES \\\\\\\\\\\\\\
+//
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
+
+///////////////////////////////////////////////////////
+//
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+  connectDB()
+})
---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,24 @@
+import { useChatStore } from "../store/useChatStore"
+import { useEffect } from 'react'
+import ChatHeader from "./ChatHeader"
+import MessageInput from "./MessageInput"
+
+const ChatContainer = () => {
+  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore()
+
+  useEffect(() => {
+    getMessages(selectedUser._id)
+  }, [selectedUser._id, getMessages])
+
+  if (isMessagesLoading) return <div>Loading...</div>
+
+  return (
+    <div calssName='flex-1 flex flex-col overflow-auto'>
+      <ChatHeader />
+      <p>Messages .... </p>
+      <MessageInput />
+    </div>
+  )
+}
+
+export default ChatContainer
